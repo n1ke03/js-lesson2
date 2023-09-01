e0df1de951cd95db5e5f66335c4f9c3fc0e56bef
Андрей Михайлов <andreymikhailov03@gmail.com>
2023-09-01 19:41:59 +0300

    Циклы, разнообразие

--- script2.js
+++ script2.js
@@ -6,18 +6,33 @@
 //     console.log("я почистил " + i + " картошку ")
 // }
 
-let sum = +prompt("сколько хочешь отложить?");
-let mounth = +prompt(" Сколько месяцев? ");
-let i = 1;
-let resultSum = 0;
+// let sum = +prompt("сколько хочешь отложить?");
+// let mounth = +prompt(" Сколько месяцев? ");
+// let i = 1; // задана переменная
+// let resultSum = 0;
 
 
-while(i<=mounth){
-    resultSum = resultSum + sum
-    i++
-  
-   if(i % 2 == 0){
-    resultSum -= 100
-   }
-   console.log(resultSum)
+// while(i<=mounth){
+//     resultSum = resultSum + sum
+//     i++
+
+//    if(i % 2 == 0){
+//     resultSum -= 100 // откладываем каждые 2 месяца по 100р
+//    }
+
+// }
+// console.log(resultSum)
+
+
+resultSum = 0;
+let mounth = 0;
+let sum = 0
+do {
+    sum = +prompt("сколько хочешь отложить?");
+    resultSum += sum
+    mounth++
+    alert(" Ты копишь уже " + mounth + " месяцев ")
+    y = confirm("Будешь ещё откладывать Да/Нет")
 }
+while (y == true)
+alert(" Ты накопил " + resultSum + " и держался " + mounth + " месяцев ")
