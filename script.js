a014750b65bfd506e1d29fbf8d8b6a63fce3100e
Андрей Михайлов <andreymikhailov03@gmail.com>
2023-09-01 20:47:01 +0300

    Функции

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-let number = 11;
-let result = number % 2 == 0 ? "чётное" : "не чётное";
-let resString = number + " является " + result;
-console.log(number + " является " + result);
-console.log(resString);
+// let number = 11;
+// let result = number % 2 == 0 ? "чётное" : "не чётное";
+// let resString = number + " является " + result;
+// console.log(number + " является " + result);
+// console.log(resString);
 
 
- x = +prompt("Введите число от 1 до 6");
- switch(x){
- case 2:
- case 4:
- case 6: alert("четное");
- break;
- case 1:
- case 3:
- case 5: alert("не чётное");
- break;
- default: alert("Вне диапазона");
- }
+//  x = +prompt("Введите число от 1 до 6");
+//  switch(x){
+//  case 2:
+//  case 4:
+//  case 6: alert("четное");
+//  break;
+//  case 1:
+//  case 3:
+//  case 5: alert("не чётное");
+//  break;
+//  default: alert("Вне диапазона");
+//  }
---
+++ script3.js
@@ -0,0 +1,9 @@
+function createRandom() {
+    return Math.round(Math.random() * 2) + 1;
+}
+x = createRandom();
+alert(x)
+
+for (i = i; i < 5; i++) {
+    console.log(createRandom())
+}
