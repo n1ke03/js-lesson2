a6579884b6c303877c212753eaf4d69c80216696
Андрей Михайлов <andreymikhailov03@gmail.com>
2023-09-01 20:52:15 +0300

    Синтаксис объявления функции

--- script3.js
+++ script3.js
@@ -1,9 +1,18 @@
-function createRandom() {
-    return Math.round(Math.random() * 2) + 1;
-}
-x = createRandom();
-alert(x)
+// function createRandom() {
+//     return Math.round(Math.random() * 2) + 1;
+// }
+// x = createRandom();
+// alert(x)
 
-for (i = i; i < 5; i++) {
-    console.log(createRandom())
-}
+// for (i = i; i < 5; i++) {
+//     console.log(createRandom())
+// }
+
+
+// function sum(a, b) {
+//     console.log(a + b)
+//     return a + b;
+// }
+// alert(sum(3, 5))
+
+
