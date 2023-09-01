603b0c181f8088e11f64aec7b96a3574ef7049a8
Андрей Михайлов <andreymikhailov03@gmail.com>
2023-09-01 18:56:01 +0300

    Функции

---
+++ script.js
@@ -0,0 +1,5 @@
+let number = 11;
+let result = number % 2 == 0 ? "чётное" : "не чётное";
+let resString = number + " является " + result;
+console.log(number + " является " + result);
+console.log(resString);
